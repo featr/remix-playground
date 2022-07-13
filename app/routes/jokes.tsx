ef462175d28c3c9f28fe4d85d512f2c1064dfc40
Evlogi Aleksandrov <aleksandrov.evlogi@gmail.com>
2022-07-13 12:24:47 +0300

    added login/logout functionality; added user model; added util functions

--- app/routes/jokes.tsx
+++ app/routes/jokes.tsx
@@ -1,16 +1,42 @@
-import { Joke } from ".prisma/client";
-import { json, LinksFunction, LoaderFunction } from "@remix-run/node";
-import { Outlet, Link, useLoaderData } from "@remix-run/react";
+import type { User } from "@prisma/client";
+import type { LinksFunction, LoaderFunction } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import { Link, Outlet, useLoaderData } from "@remix-run/react";
 
-import stylesUrl from "~/styles/jokes.css";
 import { db } from "~/utils/db.server";
+import { getUser } from "~/utils/session.server";
+import stylesUrl from "~/styles/jokes.css";
 
 export const links: LinksFunction = () => {
   return [{ rel: "stylesheet", href: stylesUrl }];
 };
 
+type LoaderData = {
+  user: {
+    id: string;
+    username: string;
+  } | null;
+  jokeListItems: Array<{ id: string; name: string }>;
+};
+
+export const loader: LoaderFunction = async ({ request }) => {
+  const jokeListItems = await db.joke.findMany({
+    take: 5,
+    orderBy: { createdAt: "desc" },
+    select: { id: true, name: true },
+  });
+  const user = await getUser(request);
+
+  const data: LoaderData = {
+    jokeListItems,
+    user,
+  };
+  return json(data);
+};
+
 export default function JokesRoute() {
   const data = useLoaderData<LoaderData>();
+
   return (
     <div className="jokes-layout">
       <header className="jokes-header">
@@ -21,6 +47,18 @@ export default function JokesRoute() {
               <span className="logo-medium">J🤪KES</span>
             </Link>
           </h1>
+          {data.user ? (
+            <div className="user-info">
+              <span>{`Hi ${data.user.username}`}</span>
+              <form action="/logout" method="post">
+                <button type="submit" className="button">
+                  Logout
+                </button>
+              </form>
+            </div>
+          ) : (
+            <Link to="/login">Login</Link>
+          )}
         </div>
       </header>
       <main className="jokes-main">
@@ -29,13 +67,11 @@ export default function JokesRoute() {
             <Link to=".">Get a random joke</Link>
             <p>Here are a few more jokes to check out:</p>
             <ul>
-              {data.jokes.map((joke, index) => {
-                return (
-                  <li key={joke.id}>
-                    <Link to={joke.id}>{joke.name}</Link>
-                  </li>
-                );
-              })}
+              {data.jokeListItems.map((joke) => (
+                <li key={joke.id}>
+                  <Link to={joke.id}>{joke.name}</Link>
+                </li>
+              ))}
             </ul>
             <Link to="new" className="button">
               Add your own
@@ -49,23 +85,3 @@ export default function JokesRoute() {
     </div>
   );
 }
-
-type JokePartial = Pick<Joke, "id" | "name">;
-
-// export interface Joke {
-//   name: string;
-//   content: string;
-// }
-
-type LoaderData = { jokes: JokePartial[] };
-
-export const loader: LoaderFunction = async () => {
-  const data: LoaderData = {
-    jokes: await db.joke.findMany({
-      take: 5,
-      select: { id: true, name: true },
-      orderBy: { createdAt: "desc" },
-    }),
-  };
-  return json(data);
-};
